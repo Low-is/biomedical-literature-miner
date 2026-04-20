d828c47857cf716499503786f6a0241abb48904b
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-20 15:18:15 -0500

    Update storage.py

--- src/storage.py
+++ src/storage.py
@@ -1,1 +1,15 @@
+import os
 
+FILE_PATH = "data/gse_ids.csv"
+
+def load_seen_ids():
+    if not os.path.exists(FILE_PATH):
+        return set()
+
+    with open(FILE_PATH, "r") as f:
+        return set(line.strip() for line in f)
+
+def save_seen_ids(ids):
+    with open(FILE_PATH, "w") as f:
+        for i in ids:
+            f.write(f"{i}\n")
