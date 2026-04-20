95cc1b46ffd033c84ed8e520f4b8b9912ea84558
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-20 15:24:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 # orchestrates workflow
 
+import yaml
 from src.search import run_search
 from src.storage import load_seen_ids, save_seen_ids
 from src.filter import get_new_ids
 
 def main():
-    search_term = (
-        '(bronchopulmonary dysplasia OR BPD) AND '
-        '(RNA-seq OR RNAseq OR microarray OR "gene expression profiling") AND '
-        '(Homo sapiens[Organism]) AND '
-        '(neonate OR newborn OR infant OR preterm)'
-    )
+    # -----------------------
+    # LOAD CONFIG
+    # -----------------------
+    with open("configs/config.yaml", "r") as f:
+        config = yaml.safe_load(f)
 
+    # -----------------------
+    # RUN SEARCH
+    # -----------------------
     print("🔍 Running weekly GEO search...")
 
     current_ids = run_search(search_term)
+    
     seen_ids = load_seen_ids()
-
     new_ids = get_new_ids(current_ids, seen_ids)
 
     print(f"Found {len(new_ids)} NEW datasets:\n")
@@ -24,7 +27,9 @@ def main():
     for gse in new_ids:
         print(f"https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc={gse}")
 
-    # Update stored IDs
+    # -----------------------
+    # SAVE STATE
+    # -----------------------
     all_ids = set(current_ids).union(seen_ids)
     save_seen_ids(all_ids)
 
