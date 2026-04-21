6bf7e81ec9e9fbd6aa87a05a187fba246cabe481
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-21 10:08:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import yaml
 from src.search import run_search
 from src.storage import load_seen_ids, save_seen_ids
 from src.filter import get_new_ids
+from src.report_writer import append_to_weekly_report
 
 def main():
     # -----------------------
@@ -23,6 +24,12 @@ def main():
     new_ids = get_new_ids(current_ids, seen_ids)
 
     print(f"Found {len(new_ids)} NEW datasets:\n")
+
+    # -----------------------
+    # SAVE REPORT
+    # -----------------------
+    append_to_weekly_report(new_ids)
+    
 
     for gse in new_ids:
         print(f"https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc={gse}")
