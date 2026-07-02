a575544f68460fb98880eb6a1d8a26dd89f7caa3
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-07-02 00:26:50 -0500

    Update search.py

--- src/search.py
+++ src/search.py
@@ -131,4 +131,4 @@ with open("configs/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 archive_rna = run_search(config["rna_archive_search"], config["email"])
-print(type(archive_rna))
+print(archive_rna["overall_design"])
